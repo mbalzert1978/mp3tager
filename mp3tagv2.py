127e5386977fa3d85b763b855838c55c78b71eed
Markus Iorio <mbalzert@gmail.com>
2022-11-19 16:18:45 +0100

    mp3tag update for modelbase

--- mp3tagv2.py
+++ mp3tagv2.py
@@ -2,26 +2,30 @@ import sys
 from pathlib import Path
 
 from src.filesystem.base import FileSystem
-from src.filesystem.mp3_tag_filesystem import MediaTagFileSystem
+from src.filesystem.media import MediaTagFileSystem
 from src.reader.base import Reader
-from src.reader.reader import AudDReader, MultiReader, TagReader
-from src.utilities.string import sanatize
+from src.reader.reader import (
+    ACRCloudReader,
+    AudDReader,
+    MultiReader,
+    TagReader,
+)
 
 
 def mp3tag(
     source_folder: Path,
     destination_folder: Path,
     file_system: FileSystem = MediaTagFileSystem(),
-    reader: Reader = MultiReader((TagReader(), AudDReader())),
+    reader: Reader = TagReader(),
 ) -> None:
     files = file_system.get(source_folder)
     for file in files:
         ext = file.suffix
-        tags = reader.read(file)
-        artist = sanatize(tags.get("artist"))
-        album = sanatize(tags.get("album"))
-        title = sanatize(tags.get("title"))
-        path = file_system.mkpath(destination_folder, (artist, album, title))
+        tags = reader.read(file).get_tags()
+        path = file_system.mkpath(
+            destination_folder,
+            (tags.get("artist"), tags.get("album"), tags.get("title")),
+        )
         file_system.mkdir(path)
         path = path.with_suffix(ext)
         file_system.copy(file, path)
